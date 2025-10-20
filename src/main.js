5e1f8b795ac8766e651a403980fedf5f635d598b
Darwin Iquina (Laptop) <darwiniquina@hierpayroll.com>
2025-10-20 15:47:11 +0800

    feat: finalize all pending section and add animation

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ gsap.registerPlugin(SplitText, ScrollTrigger, ScrollSmoother, ScrambleTextPlugin
 
 document.addEventListener("DOMContentLoaded", (event) => {
 
-    ScrollSmoother.create({
-        smooth: 2,
+    let smoother = ScrollSmoother.create({
+        smooth: 3,
         effects: true,
         smoothTouch: 0.1,
     });
@@ -28,7 +28,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
             start: "top 70%",
             endTrigger: "body",
             end: "bottom bottom",
-            markers: true,
             toggleActions: "play none none reverse",
         }
     })
@@ -65,10 +64,111 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     gsap.from(stoicParagraphSplit.lines, {
         y: 100, ease: "power4.inOut", stagger: 0.5,
-        scrollTrigger: { trigger: '#stoicism .paragraph', start: "top 80%", endTrigger: "#stoicism", end: "bottom bottom", scrub: true, }
+        scrollTrigger: { trigger: '#stoicism .paragraph', start: "top 80%", endTrigger: "#stoicism", end: "90% bottom", scrub: true, }
+    })
+
+    // EPICTETUS TIME LINE
+
+    gsap.from('.epictetus-left', {
+        xPercent: -100, filter: "blur(10px)", stagger: 0.5,
+        scrollTrigger: { trigger: ".epictetus-left", start: "top 90%", endTrigger: "#epictetus", end: "80% bottom", scrub: true, }
+    })
+
+    gsap.from('.epictetus-center', {
+        yPercent: 100, filter: "blur(10px)", stagger: 0.5,
+        scrollTrigger: { trigger: "#epictetus", start: "top 90%", endTrigger: "#epictetus", end: "80% bottom", scrub: true }
+    })
+
+    gsap.from('.epictetus-right', {
+        xPercent: 100, filter: "blur(10px)", stagger: 0.5,
+        scrollTrigger: { trigger: ".epictetus-right", start: "top 90%", endTrigger: "#epictetus", end: "80% bottom", scrub: true, }
     })
 
 
+    //  SENECA
 
+    let senecaTextSplit = SplitText.create('#seneca h1', { type: 'chars', mask: "chars", autoSplit: true })
+    let senecaParagraphSplit = SplitText.create('#seneca p', { type: 'chars', mask: "chars", autoSplit: true })
+
+    gsap.from(senecaTextSplit.chars, {
+        filter: "blur(10px)", y: gsap.utils.wrapYoyo([-100, 100]), ease: "power4.inOut",
+        stagger: { each: 0.06, from: "random", },
+        scrollTrigger: { trigger: "#seneca", start: "top 80%", endTrigger: "#seneca", end: "80% bottom", scrub: true, }
+    })
+
+    gsap.from(senecaParagraphSplit.chars, {
+        filter: "blur(10px)", y: gsap.utils.wrapYoyo([-100, 100]), ease: "back", stagger: { each: 0.06, from: "random", },
+        scrollTrigger: { trigger: '#seneca p', start: "top 80%", endTrigger: "#seneca", end: "bottom bottom", scrub: true, }
+    })
+
+    gsap.from("#seneca img", {
+        yPercent: 200, filter: "blur(10px)",
+        scrollTrigger: { trigger: "#seneca", start: "top 80%", endTrigger: "#seneca", end: "90% bottom", scrub: true, }
+    })
+
+    // MARCUS
+    let marcusTextSplit = SplitText.create('#marcus h1', { type: 'lines', mask: "lines", autoSplit: true })
+    let marcusParagraphSplit = SplitText.create('#marcus p', { type: 'lines', mask: "lines", autoSplit: true })
+
+    gsap.from(marcusTextSplit.lines, {
+        filter: "blur(10px)", y: gsap.utils.wrapYoyo([-100, 100]), ease: "back", stagger: { each: 0.06, from: "random", },
+        scrollTrigger: { trigger: "#marcus h1", start: "top 80%", endTrigger: "#marcus", end: "80% bottom", scrub: true, }
+    })
+
+    gsap.from(marcusParagraphSplit.lines, {
+        y: 100, ease: "power4.inOut", stagger: 0.5,
+        scrollTrigger: { trigger: '#marcus p', start: "top 80%", endTrigger: "#marcus", end: "bottom bottom", scrub: true }
+    })
+
+    gsap.from("#marcus img", {
+        yPercent: 200, filter: "blur(10px)",
+        scrollTrigger: { trigger: "#marcus", start: "top 80%", endTrigger: "#marcus", end: "90% bottom", scrub: true, }
+    })
+
+    // FOOTER PAGE
+
+    let thankyouPopup = document.querySelector(".thankyou-popup")
+
+    gsap.from(thankyouPopup, {
+        opacity: 0,
+        duration: 0.5,
+        scrollTrigger: {
+            trigger: ".thankyou-placeholder",
+            start: "top bottom",
+            toggleActions: "play none none reverse",
+        }
+
+    })
+
+    thankyouPopup.addEventListener("click", (e) => {
+        smoother.scrollTo("#hero", true, "center center")
+    })
+
+    // NAVIGATIONS
+
+    let navStoic = document.querySelector(".nav-stoic")
+    let navEpic = document.querySelector(".nav-epic")
+    let navSenec = document.querySelector(".nav-senec")
+    let navMarcus = document.querySelector(".nav-marcus")
+
+    navStoic.addEventListener("click", (e) => {
+        smoother.scrollTo("#stoicism", true, "center center")
+        e.preventDefault()
+    })
+
+    navEpic.addEventListener("click", (e) => {
+        smoother.scrollTo("#epictetus", true, "center center")
+        e.preventDefault()
+    })
+
+    navSenec.addEventListener("click", (e) => {
+        smoother.scrollTo("#seneca", true, "center center")
+        e.preventDefault()
+    })
+
+    navMarcus.addEventListener("click", (e) => {
+        smoother.scrollTo("#marcus", true, "center center")
+        e.preventDefault()
+    })
 });
 
